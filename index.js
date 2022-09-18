62715f256082f16d3ffb2bc160187dc82baec9b0
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-17 21:17:33 -0700

    test bing search api

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ app.get("/dinoname", async (request, response) => {
     "https://dinoipsum.com/api/?format=json&words=2&paragraphs=1"
   );
   const dinoNameResponse = await fetchApi.json();
-    // console.log(dinoNameResponse);
+  // console.log(dinoNameResponse);
   response.json(dinoNameResponse);
+});
+
+app.get("/dinoImage", async (request, response) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "f95dacf387msh6f0d149205e4e5ap1fd2a5jsn211e1f8ce304",
+      "X-RapidAPI-Host": "bing-image-search1.p.rapidapi.com",
+    },
+  };
+  const fetchApi = await fetch(
+    "https://bing-image-search1.p.rapidapi.com/images/search?q=dinosaur&count=10",
+    options
+  );
+  const dinoImageResponse = await fetchApi.json();
+  console.log(dinoImageResponse);
+  response.json(dinoImageResponse);
 });
--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
-getDinoName();
+document.querySelector("#btnLoad").addEventListener("click", () => {
+  getDinoName();
+  getDinoImage();
+});
 
 async function getDinoName() {
   const response = await fetch("./dinoname");
   const data = await response.json();
   let dinoName = data[0].join(" ");
   console.log(dinoName);
+}
+
+async function getDinoImage() {
+  const response = await fetch("./dinoImage");
+  const data = await response.json();
+  let donoImage = data.value[0].thumbnailUrl;
+  console.log(donoImage);
 }
